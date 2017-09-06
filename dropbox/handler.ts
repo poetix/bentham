41f775b459e4b83db74871050d617160d9953a1e
Dominic Fox <dominic.fox@wipro.com>
2017-09-06 12:13:30 +0100

    Challenge webhook

--- dropbox/handler.ts
+++ dropbox/handler.ts
@@ -1,3 +1,20 @@
-export const hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!', event }
-);
+export const webhookChallenge = (event, context, cb) => {
+  console.log(event)
+
+  cb(null,
+    {
+      statusCode: 200,
+      body: event.query.challenge
+    })
+}
+
+export const webhookNotify = (event, context, cb) => cb(null,
+  processNotification(JSON.parse(event.body))
+)
+
+function processNotification(notification) {
+    console.log(notification)
+    return {
+      statusCode: 200
+    }
+}
