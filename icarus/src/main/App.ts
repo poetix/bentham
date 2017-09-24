4f2e6b3b3d0cb84e7525a94833c7c8d469c0a39d
Dominic Fox <dominic.fox@wipro.com>
2017-09-24 22:49:10 +0100

    Fix up App

--- icarus/src/main/App.ts
+++ icarus/src/main/App.ts
@@ -3,11 +3,32 @@ Wires everything together
 */
 
 import { HttpSlackClient } from "./clients/Slack";
+import { LoginService, UserToken } from "./services/ServiceApi";
 import { SlackLoginEndpoint } from "./endpoints/SlackLoginEndpoint";
 
 const slackClient = new HttpSlackClient(
   process.env.SLACK_CLIENT_ID,
   process.env.SLACK_CLIENT_SECRET);
 
+// services
+class StubLoginService implements LoginService {
+
+  async login(slackCode: string): Promise<UserToken> {
+    return {
+      accessToken: "the access token",
+      identities: {
+        slack: {
+          id: "the slack id",
+          teamId: "the slack team id",
+          userName: "Arthur Putey",
+          accessToken: "the slack access token"
+        }
+      }
+    };
+  }
+}
+
+const loginService = new StubLoginService();
+
 // Endpoints
-export const slackLoginEndpoint = new SlackLoginEndpoint(slackClient);
+export const slackLoginEndpoint = new SlackLoginEndpoint(loginService);
